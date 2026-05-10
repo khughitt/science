2ac1102544b4a480883e2a2e6621d3d64ea352e6
Keith Hughitt <keith.hughitt@gmail.com>
2026-05-10 00:12:19 -0400

    feat(wander): re-export public API from package init

--- science/src/science_tool/wander/__init__.py
+++ science/src/science_tool/wander/__init__.py
@@ -1,5 +1,26 @@
 from __future__ import annotations
 
+from science_tool.wander.cli import wander_command
+from science_tool.wander.context import ContextBundle, assemble_bundle
+from science_tool.wander.neighbors import NeighborEdge, NeighborSet, neighbors_for
+from science_tool.wander.references import Reference, active_references_for
 from science_tool.wander.sampling import WanderSamplerError, sample_for_walk
+from science_tool.wander.skeleton import render_json, render_markdown_skeleton
+from science_tool.wander.stub_smell import StubSignals, compute_stub_signals
 
-__all__ = ["WanderSamplerError", "sample_for_walk"]
+__all__ = [
+    "ContextBundle",
+    "NeighborEdge",
+    "NeighborSet",
+    "Reference",
+    "StubSignals",
+    "WanderSamplerError",
+    "active_references_for",
+    "assemble_bundle",
+    "compute_stub_signals",
+    "neighbors_for",
+    "render_json",
+    "render_markdown_skeleton",
+    "sample_for_walk",
+    "wander_command",
+]
